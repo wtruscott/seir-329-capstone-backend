d4a23a393ea97180eb5d668190d0d03e425f6e7f
William Truscott <williamtruscott@Williams-MacBook-Pro.local>
2021-06-20 09:49:17 -0400

    refactored many things

--- things/admin.py
+++ things/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
+from .models import Thing, Container, Place, Collection
 
 # Register your models here.
 
 class ThingAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ('title',)}
+    prepopulated_fields = {'slug': ('name',)}
+
+admin.site.register(Thing, ThingAdmin)
+
+class CollectionAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+admin.site.register(Collection, CollectionAdmin)
+
+class ContainerAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+admin.site.register(Container, ContainerAdmin)
+
+class PlaceAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+admin.site.register(Place, PlaceAdmin)
--- things/models.py
+++ things/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.utils.text import slugify
+# from capstone_backend.utils import unique_slug_generator
+# from django.db.models.signals import pre_save
 
 # Create your models here.
 
@@ -9,23 +10,51 @@ class Thing(models.Model):
     location = models.CharField(max_length=100)
     owner = models.CharField(max_length=100)
     favorite = models.BooleanField(default=False)
-    slug =models.SlugField(unique=True)
+    slug =models.SlugField(max_length=250, null=True, blank=True)
 
-    def save(self, *args, **kwargs):
-        self.slug = self.slug or slugify(self.name)
-        super().save(*args, **kwargs)
-    
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+# def slug_generator(sender, instance, *args, **kwargs):
+#     if not instance.slug:
+#         instance.slug = unique_slug_generator(instance)
+
+# pre_save.connect(slug_generator, sender=Thing)
 
 class Container(models.Model):
     name = models.CharField(max_length=100)
-    things = models.ManyToManyField(Thing)
+    things = models.ManyToManyField(Thing, blank=True)
+    slug =models.SlugField(max_length=250, null=True, blank=True)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
 
 class Place(models.Model):
     name = models.CharField(max_length=100)
-    containers = models.ManyToManyField(Container)
-    things = models.ManyToManyField(Thing)
+    containers = models.ManyToManyField(Container, blank=True)
+    things = models.ManyToManyField(Thing, blank=True)
+    slug =models.SlugField(max_length=250, null=True, blank=True)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
     
 
 class Collection(models.Model):
     name = models.CharField(max_length=100)
-    things = models.ManyToManyField(Thing)
+    things = models.ManyToManyField(Thing, blank=True)
+    slug =models.SlugField(max_length=250, null=True, blank=True)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
